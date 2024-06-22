8c959eea04e1b459bac2d4ba6b097c7fd4a551d7
Jacob Nardone <xjtn18@gmail.com>
2024-06-22 01:49:53 +0000

    Renamed install scripts; refactored unix install script and included bashrc; removed .gitignore

---
+++ unix_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Pull the latest changes from git
+git pull
+
+# Set DOTFILES_DIR to the directory of the script
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Create symbolic links (soft links) for the files
+ln -sf "${DOTFILES_DIR}/.bashrc" "$HOME/.bashrc"
+
+ln -sf "${DOTFILES_DIR}/.tmux.conf" "$HOME/.tmux.conf"
+
+ln -sf "${DOTFILES_DIR}/.rgignore" "$HOME/.rgignore"
+
+mkdir -p "$HOME/.config/lazygit"
+ln -sf "${DOTFILES_DIR}/lazygit.yml" "$HOME/.config/lazygit/config.yml"
+
+rm -rf "$HOME/.config/nvim"
+ln -s "${DOTFILES_DIR}/nvim" "$HOME/.config/nvim"
+
+echo "Dotfiles are now synced."
