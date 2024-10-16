5aa1bbdfd0bab046769ec30b740c5ee883fb8b15
Jacob Nardone <jacob@spartan.local>
2024-10-16 15:38:32 -0700

    zshrc + mac_install

---
+++ mac_install.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+# Pull the latest changes from git
+git pull
+
+# Set DOTFILES_DIR to the directory of the script
+DOTFILES_DIR="$( cd "$( dirname "${(%):-%x}" )" && pwd )"
+
+# Create symbolic links (soft links) for the files
+ln -sf "${DOTFILES_DIR}/.bashrc" "$HOME/.bashrc"
+
+ln -sf "${DOTFILES_DIR}/.tmux.conf" "$HOME/.tmux.conf"
+
+ln -sf "${DOTFILES_DIR}/.rgignore" "$HOME/.rgignore"
+
+mkdir -p "$HOME/.config/lazygit"
+ln -sf "${DOTFILES_DIR}/lazygit.yml" "$HOME/.config/lazygit/config.yml"
+
+rm -rf "$HOME/.config/nvim"
+ln -s "${DOTFILES_DIR}/nvim" "$HOME/.config/nvim"
+
+ln -s "${DOTFILES_DIR}/.inputrc" "$HOME/.inputrc"
+
+echo "Dotfiles are now synced."
